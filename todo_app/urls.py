b7c7a7b8a9f704633621c685163c672b846311fc
aswin <aswinpdas83@gmail.com>
2022-06-09 16:44:49 +0530

    small change

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from todo_app import views
 
-urlpatterns=[
-    path('',views.task_view,name='task_view'),
-    # path('task',views.task,name='task')
+urlpatterns = [
+    path('', views.task_view, name='task_view'),
+    path('task',views.task_,name='task')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
@@ -13,9 +14,9 @@ from todo_app.models import Task
 #     return  render(req,"task.html",{})
 def task_view(req):
     if req.method == 'POST':
-        name=req.POST.get('name')
-        priority=req.POST.get('priority')
-        obj_task=Task(name=name,priority=priority)
-        obj_task.save()
-    obj_task = Task.objects.all()
-    return render(req,"task_view.html",{'obj_task':obj_task})
+        name = req.POST.get('name')
+        priority = req.POST.get('priority')
+        obj_task_1 = Task(name=name, priority=priority)
+        obj_task_1.save()
+    obj_task_2 = Task.objects.all()
+    return render(req, "task_view.html", {'obj_task': obj_task_2})
