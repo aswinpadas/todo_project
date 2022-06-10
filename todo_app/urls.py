41e42eb999fbe39f2df19a835886ba2a82282f99
aswin <aswinpdas83@gmail.com>
2022-06-10 23:57:17 +0530

    multi delete feature completed, with javascript in task_view.html

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from todo_app import views
 
 urlpatterns = [
-    path('', views.task_view, name='task_view'),
     path('delete/<int:id>/', views.delete, name='delete'),
-    path('multi_delete/',views.multi_delete,name='multi_delete'),
+    path('multi_delete/<str:id_list_string>/',views.multi_delete,name='multi_delete'),
+    path('', views.task_view, name='task_view')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -14,7 +14,7 @@ from todo_app.models import Task
 #     return  render(req,"task.html",{})
 def task_view(req):
     if req.method == 'POST':
-        print('request is',req.POST)
+        print('request is', req.POST)
         name = req.POST.get('name')
         priority = req.POST.get('priority')
         obj_task_1 = Task(name=name, priority=priority)
@@ -24,21 +24,24 @@ def task_view(req):
     obj_task_2 = Task.objects.all()
 
     return render(req, "task_view.html", {'obj_task': obj_task_2})
-def delete(req,id):
+
+
+def delete(req, id):
     obj_task = Task.objects.get(id=id)
-    if req.method=='POST':
+    if req.method == 'POST':
         obj_task.delete()
-        return  redirect('/')
-    return render(req,"delete.html",{'obj_task':obj_task})
-def multi_delete(req):
+        return redirect('/')
+    return render(req, "delete.html", {'obj_task': obj_task})
+
+
+def multi_delete(req, id_list_string):
     print('****************************************')
-    if req.method=='POST':
-        print(req)
-        id_list = req.POST.getlist('checks[]')
+    if req.method == 'GET':
+        id_list = id_list_string.split(',');
         print(id_list)
-        for id in id_list:
-            obj_task=Task.objects.get(id=id)
+        for eid in id_list:
+            obj_task = Task.objects.get(id=eid)
             obj_task.delete()
         return redirect('/')
     else:
-        return render(req,"multi_delete.html")
+        return render(req, "multi_delete.html")
